f9017371d64854a553e9321cd9d306515e57badc
Akash Kundu <aakassh.uit@gmail.com>
2024-02-08 20:42:11 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function showDate() {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1; // Months are zero indexed, so add 1
+    let year = date.getFullYear();
+
+    let fullDate = day + "/" + month + "/" + year;
+
+    document.getElementById("MyDateDisplay").innerText = fullDate;
+    document.getElementById("MyDateDisplay").textContent = fullDate;
+}
+
+function formatTime(time) {
+    return time < 10 ? "0" + time : time;
+}
+
+showDate();
+
 function showTime(){
     var date = new Date();
     var h = date.getHours(); 
